d8ce82b13d2ec1d388be39fea2cf79869430c8a4
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-01-30 20:07:59 +0100

    formularios

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 
 import { JsonPipe } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   standalone: true,
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './basic-page.component.html',
   styles: ``
 })
+
+
 export class BasicPageComponent {
+
+ /*  public myForm: FormGroup = new FormGroup ({
+    name: new FormControl ("", [], []),
+    price: new FormControl (0, [], []),
+    inStorage: new FormControl (0, [], []),
+
+  }) */
+
+  public myForm: FormGroup = this.fb.group({
+    name: [""],
+    price: [0],
+    inStorage: [0]
+  })
+
+  constructor (private fb: FormBuilder) {}
+
+  onSave():void {
+    console.log(this.myForm.value);
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
